72c459c1a217fcfdefb24ef4ec22cbcb44e68467
Arun C Mohanan <aruncmunni@gmail.com>
2024-03-18 13:23:29 +0530

    updated some changes api

--- API/pkg/di/wire.go
+++ API/pkg/di/wire.go
@@ -1,62 +1,63 @@
+
 package di
 
-import (
-	http "WatchHive/pkg/api"
-	"WatchHive/pkg/api/handler"
-	"WatchHive/pkg/config"
-	"WatchHive/pkg/db"
-	"WatchHive/pkg/helper"
-	"WatchHive/pkg/repository"
-	"WatchHive/pkg/usecase"
+// import (
+// 	http "WatchHive/pkg/api"
+// 	"WatchHive/pkg/api/handler"
+// 	"WatchHive/pkg/config"
+// 	"WatchHive/pkg/db"
+// 	"WatchHive/pkg/helper"
+// 	"WatchHive/pkg/repository"
+// 	"WatchHive/pkg/usecase"
 
-	"github.com/google/wire"
-)
+// 	"github.com/google/wire"
+// )
 
-func InitializeAPI(cfg config.Config) (*http.ServerHTTP, error) {
-	wire.Build(
+// func InitializeAPI(cfg config.Config) (*http.ServerHTTP, error) {
+// 	wire.Build(
 
-		db.ConnectDatabase,
+// 		db.ConnectDatabase,
 
-		repository.NewUserRepository,
-		repository.NewAdminRepository,
-		repository.NewOtpRepository,
-		repository.NewCategoryRepository,
-		repository.NewProductRepository,
-		repository.NewCartRepository,
-		repository.NewOrderRepository,
-		repository.NewPaymentRepository,
-		repository.NewWalletRepository,
-		repository.NewOfferRepository,
-		repository.NewCouponRepository,
+// 		repository.NewUserRepository,
+// 		repository.NewAdminRepository,
+// 		repository.NewOtpRepository,
+// 		repository.NewCategoryRepository,
+// 		repository.NewProductRepository,
+// 		repository.NewCartRepository,
+// 		repository.NewOrderRepository,
+// 		repository.NewPaymentRepository,
+// 		repository.NewWalletRepository,
+// 		repository.NewOfferRepository,
+// 		repository.NewCouponRepository,
 
-		usecase.NewUserUseCase,
-		usecase.NewAdminUseCase,
-		usecase.NewOtpUseCase,
-		usecase.NewCategoryUseCase,
-		usecase.NewProductUseCase,
-		usecase.NewCartUseCase,
-		usecase.NewOrderUseCase,
-		usecase.NewPaymentUseCase,
-		usecase.NewWalletUsecase,
-		usecase.NewOferUsecase,
-		usecase.NewCouponUsecase,
+// 		usecase.NewUserUseCase,
+// 		usecase.NewAdminUseCase,
+// 		usecase.NewOtpUseCase,
+// 		usecase.NewCategoryUseCase,
+// 		usecase.NewProductUseCase,
+// 		usecase.NewCartUseCase,
+// 		usecase.NewOrderUseCase,
+// 		usecase.NewPaymentUseCase,
+// 		usecase.NewWalletUsecase,
+// 		usecase.NewOferUsecase,
+// 		usecase.NewCouponUsecase,
 
-		handler.NewUserHandler,
-		handler.NewAdminHandler,
-		handler.NewOtpHandler,
-		handler.NewCategoryHandler,
-		handler.NewProductHandler,
-		handler.NewCartHandler,
-		handler.NewOrderHandler,
-		handler.NewPaymentHandler,
-		handler.NewWalletHandler,
-		handler.NewOfferHandler,
-		handler.NewCouponHandler,
+// 		handler.NewUserHandler,
+// 		handler.NewAdminHandler,
+// 		handler.NewOtpHandler,
+// 		handler.NewCategoryHandler,
+// 		handler.NewProductHandler,
+// 		handler.NewCartHandler,
+// 		handler.NewOrderHandler,
+// 		handler.NewPaymentHandler,
+// 		handler.NewWalletHandler,
+// 		handler.NewOfferHandler,
+// 		handler.NewCouponHandler,
 
-		helper.NewHelper,
+// 		helper.NewHelper,
 
-		http.NewServerHTTP,
-	)
+// 		http.NewServerHTTP,
+// 	)
 
-	return &http.ServerHTTP{}, nil
-}
+// 	return &http.ServerHTTP{}, nil
+// }
